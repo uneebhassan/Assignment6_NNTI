cbbc6583cac13b338497faafd923649e64481713
neuronet_team213 neuronet_team213 <neuronet_team213@conduit.hpc.uni-saarland.de>
2023-12-14 23:34:49 +0100

    updated

--- torch_matmul_docker.py
+++ torch_matmul_docker.py
@@ -1,16 +1,88 @@
-#!/usr/bin/env python3
-import sys
 import torch
-import time
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torchvision import transforms
+from torch.utils.data import DataLoader, random_split
 
-print("Running python version: ", sys.version)
-print("PyTorch version :", torch.__version__)
-print("GPU Available: ", torch.cuda.is_available())
-print("Num GPUs Available: ", torch.cuda.device_count())
+# Set random seed for reproducibility
+torch.manual_seed(42)
 
+# Define transformations
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
-matrix1 = torch.tensor([1.0, 2.0, 3.0, 4.0]).reshape(2, 2)
-matrix2 = torch.linalg.inv(matrix1)
-product = matrix1 @ matrix2
+# Load FashionMNIST dataset
+train_dataset = torchvision.datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = torchvision.datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
 
-print(product)
+# Split the dataset into train, validation, and test sets
+train_size = int(0.8 * len(train_dataset))
+val_size = len(train_dataset) - train_size
+
+train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
+
+# Create DataLoader for train, validation, and test sets
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+# Define the neural network model
+class SimpleNN(nn.Module):
+    def __init__(self, input_size, hidden_size1, hidden_size2, output_size):
+        super(SimpleNN, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size1)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(hidden_size2, output_size)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        return x
+
+# Instantiate the model
+input_size = 28 * 28  # Size of FashionMNIST images
+hidden_size1 = 128
+hidden_size2 = 64
+output_size = 10  # Number of classes in FashionMNIST
+model = SimpleNN(input_size, hidden_size1, hidden_size2, output_size)
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# Train the model for 10 epochs
+num_epochs = 10
+for epoch in range(num_epochs):
+    model.train()
+    for inputs, labels in train_loader:
+        inputs = inputs.view(-1, input_size)
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+# Evaluate the model on train, validation, and test sets
+def evaluate(loader, name):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in loader:
+            inputs = inputs.view(-1, input_size)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy = correct / total
+    print(f'Accuracy on {name} set: {100 * accuracy:.2f}%')
+
+evaluate(train_loader, 'train')
+evaluate(val_loader, 'validation')
+evaluate(test_loader, 'test')
